d1aa53ce0aafdd425147a8a970953d7bf025b559
Matt Lawrence <m3lawren@uwaterloo.ca>
2008-12-13 00:28:53 -0500

    Fixing up some formatting issues.
    Signed-off-by: Matt Lawrence <m3lawren@uwaterloo.ca>

--- pymazon.py
+++ pymazon.py
@@ -19,17 +19,21 @@ def main():
 	config = ConfigParser.SafeConfigParser()
 	if config.read('pymazon.conf') == None:
 		print 'Unable to read configuration file pymazon.conf'
-		sys.exit(1)	
+		sys.exit(1)
 
 	if config.has_option('pymazon', 'AWSAccessKeyId') == False:
 		print 'Missing option AWSAccessKeyId in the pymazon section'
 		sys.exit(1)
 
 	key = config.get('pymazon', 'AWSAccessKeyId')
+	request = 'http://webservices.amazon.com/onca/xml?' + \
+	          'Service=AWSECommerceService' + \
+				 '&AWSAccessKeyId=' + key + \
+				 '&Operation=ItemLookup' + \
+				 '&ResponseGroup=ItemAttributes' + \
+				 '&ItemId=0618669078'
 
-	request = 'http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&AWSAccessKeyId=' + key + '&Operation=ItemLookup&ResponseGroup=ItemAttributes&ItemId=0618669078'
 	file = urllib.urlretrieve(request)[0]
-
 	xmldoc = minidom.parse(file)
 
 	errors = xmldoc.getElementsByTagName('Errors')
@@ -41,13 +45,12 @@ def main():
 			dumpelem('Message', error)
 
 		sys.exit(1)
-		
+
 	dumpelem('Title', xmldoc)
 	dumpelem('Author', xmldoc)
 	dumpelem('ISBN', xmldoc)
 	dumpelem('Edition', xmldoc)
 	dumpelem('PublicationDate', xmldoc)
-	dumpelem('NumberOfPages', xmldoc)
 	dumpelem('NumberOfPages', xmldoc)
 
 if __name__ == '__main__':
